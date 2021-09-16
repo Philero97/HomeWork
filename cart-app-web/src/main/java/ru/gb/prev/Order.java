8c6d304a080677d3210434871537949b5d093ad2
Philero97 <69924342+Philero97@users.noreply.github.com>
2021-09-16 08:44:31 +0300

    3HW

---
+++ cart-app-web/src/main/java/ru/gb/prev/Order.java
@@ -0,0 +1,52 @@
+package ru.gb.prev;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Order {
+    private int id;
+    private LocalDate date;
+    private float cost;
+    private List<Product> products;
+
+    public Order() {
+
+    }
+
+    public Order(int id, LocalDate date, float cost, List<Product> products) {
+        this.id = id;
+        this.date = date;
+        this.cost = cost;
+        this.products = products;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", date=" + date +
+                ", cost=" + cost +
+                ", products=" + products +
+                '}';
+    }
+}
---
+++ cart-app-web/src/main/java/ru/gb/prev/OrderRepository.java
@@ -0,0 +1,38 @@
+package ru.gb.prev;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+public class OrderRepository {
+    private List<Order> orders = new ArrayList<>();
+
+    public OrderRepository() {
+        orders.add(     new Order(1, LocalDate.now(), 417.42F, Collections.emptyList())
+
+        );
+    }
+
+    public Optional<Order> findById(int id) {
+        return orders.stream().filter(p -> p.getId() == id).findFirst();
+    }
+
+    public List<Order> findAll() {
+        return orders;
+    }
+
+    public void add(Order order) {
+        this.orders.add(order);
+    }
+
+    @Override
+    public String toString() {
+        return "OrderRepository{" +
+                "orders=" + orders +
+                '}';
+    }
+}
+
+
